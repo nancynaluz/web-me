57af2fd23b5d015e891148d6ba7718bdb9770151
nancynaluz <nancy.naluz@glossier.com>
2022-02-14 17:11:40 -0500

    Fix server port.

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-// const express = require('express')
-// const serveStatic = require('serve-static')
-// const path = require('path')
-// app = express()
-// app.use(serveStatic(__dirname + "/dist"))
-// var port = process.env.PORT || 8080
-// app.listen(port)
-// console.log(`server started ${port}`)
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+app = express()
+app.use(serveStatic(__dirname + "/dist"))
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log(`server started ${port}`)
 
-var path = require('path');
-var express = require('express');
-
-var app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
-
-var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
-});
