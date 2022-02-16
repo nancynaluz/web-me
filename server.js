4ea9dd0248b3b480c38c430339dda0505ca1d4b5
nancynaluz <nancy.naluz@glossier.com>
2022-02-15 22:22:15 -0500

    Update PORT.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const serveStatic = require('serve-static')
 const path = require('path')
 app = express()
 app.use(serveStatic(__dirname + "/dist"))
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 80
 app.listen(port)
 console.log(`server started ${port}`)
 
